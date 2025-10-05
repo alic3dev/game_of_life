72646729dc7755b78d4c27113e1927eeec76638f
alic3 <alic3dev@gmail.com>
2025-10-05 12:25:29 -0700

    `gol_%`->`game_of_life_%`

--- include/game_of_life_parameters.h
+++ include/game_of_life_parameters.h
@@ -1,19 +1,18 @@
-#ifndef __gol_parameters_h
-#define __gol_parameters_h
+#ifndef __game_of_life_parameters_h
+#define __game_of_life_parameters_h
 
 #include <cexil.h>
 
-struct gol_parameters {
+struct game_of_life_parameters {
   struct cexil_size size_renderer;
   struct cexil_size size_offset;
   float rate_frames;
 };
 
-unsigned char gol_parameters_parse(
-  struct gol_parameters*,
+unsigned char game_of_life_parameters_parse(
+  struct game_of_life_parameters*,
   int,
   char**
 );
 
 #endif
-
