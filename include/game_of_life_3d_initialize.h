a0b1ec43c29f131d104a5271621bfd49490839c5
alic3 <alic3dev@gmail.com>
2025-10-07 16:54:39 -0700

    `game_of_life_3d_initialize`:add

---
+++ include/game_of_life_3d_initialize.h
@@ -0,0 +1,12 @@
+#ifndef __game_of_life_3d_h
+#define __game_of_life_3d_h
+
+#include <game_of_life_parameters.h>
+
+int game_of_life_3d_initialize(
+  int,
+  const char**,
+  struct game_of_life_parameters
+);
+
+#endif
