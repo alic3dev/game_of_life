d5714d3c4c5c62c2003aaa3a83e4cf06de084cf8
alic3 <alic3dev@gmail.com>
2025-10-07 14:41:19 -0700

    `game_of_life_poll`:add

---
+++ include/game_of_life_poll.h
@@ -0,0 +1,12 @@
+#ifndef __game_of_life_poll_h
+#define __game_of_life_poll_h
+
+#include <game_of_life_parameters.h>
+
+void game_of_life_poll(
+  struct game_of_life_parameters*,
+  char**,
+  char**
+);
+
+#endif
