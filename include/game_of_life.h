c87aa4c1280e5992d170942d3c9b024b8a0856c2
alic3 <alic3dev@gmail.com>
2025-10-08 10:24:56 -0700

    `game_of_life`:split_metal_accelerated_modes

--- include/game_of_life.h
+++ include/game_of_life.h
@@ -1,9 +1,31 @@
 #ifndef __game_of_life_h
 #define __game_of_life_h
 
+#if rendering_mode == 2
+#include <game_of_life_parameters.h>
+
+#include <cexil.h>
+
+#if with_metal == 1
+#include <game_of_life_metal_acceleration_data.h>
+#endif
+#endif
+
 int main(
   int,
   const char**
 );
+
+#if rendering_mode == 2
+void game_of_life_destroy(
+  struct cexil_renderer*,
+  struct game_of_life_parameters*,
+  char**
+  #if with_metal == 1
+  ,
+  struct game_of_life_metal_acceleration_data*
+  #endif
+);
+#endif
 
 #endif
--- sources/game_of_life.c
+++ sources/game_of_life.c
@@ -33,6 +33,10 @@ int main(
 }
 
 #elif rendering_mode == 2
+#if with_metal == 1
+#include <game_of_life_metal_acceleration.h>
+#include <game_of_life_metal_acceleration_data.h>
+#endif
 
 #include <cexil.h>
 #include <interrupt_handler.h>
@@ -60,7 +64,11 @@ int main(
 
   interrupt_handler_initialize();
 
-  srand(time((void*)0));
+  srand(
+    time(
+      (void*)0
+    )
+  );
 
   struct cexil_renderer renderer;
   struct cexil_size size_renderer = {
@@ -88,7 +96,11 @@ int main(
       index_x < game_of_life_parameters.size.x;
       ++index_x
     ) {
-      renderer.pixels[index_y][index_x] = (
+      renderer.pixels[
+        index_y
+      ][
+        index_x
+      ] = (
         rand() % 10 > 7 ? 1 : 0
       );
     }
@@ -104,14 +116,49 @@ int main(
     index_y < game_of_life_parameters.size.y;
     ++index_y
   ) {
-    cells_next[index_y] = malloc(
+    cells_next[
+      index_y
+    ] = malloc(
       sizeof(unsigned char) *
       game_of_life_parameters.size.x
     );
   }
 
+  #if with_metal == 1
+  struct game_of_life_metal_acceleration_data game_of_life_metal_acceleration_data = {
+    .metal_device = (void*)0,
+    .library = (void*)0,
+    .function_compute = (void*)0,
+    .pipeline_state_compute = (void*)0,
+    .error = game_of_life_metal_acceleration_data_error_none
+  };
+
+  game_of_life_metal_acceleration_initialize(
+    &game_of_life_metal_acceleration_data
+  );
+
+  if (
+    game_of_life_metal_acceleration_data.error != game_of_life_metal_acceleration_data_error_none
+  ) {
+    game_of_life_metal_acceleration_data_error_print(
+      game_of_life_metal_acceleration_data.error
+    );
+
+    return game_of_life_metal_acceleration_data.error;
+
+    game_of_life_destroy(
+      &renderer,
+      &game_of_life_parameters,
+      cells_next,
+      &game_of_life_metal_acceleration_data
+    );
+  }
+  #endif
+
   while (interrupt_handler_interrupted == 0) {
-    cexil_renderer_render(&renderer);
+    cexil_renderer_render(
+      &renderer
+    );
 
     game_of_life_poll(
       &game_of_life_parameters,
@@ -120,9 +167,53 @@ int main(
     );
   }
 
-  cexil_renderer_destroy(&renderer);
+  game_of_life_destroy(
+    &renderer,
+    &game_of_life_parameters,
+    cells_next
+    #if with_metal == 1
+    ,
+    &game_of_life_metal_acceleration_data
+    #endif
+  );
 
   return 0;
+}
+
+void game_of_life_destroy(
+  struct cexil_renderer* renderer,
+  struct game_of_life_parameters* game_of_life_parameters,
+  char** cells_next
+  #if with_metal == 1
+  ,
+  struct game_of_life_metal_acceleration_data* game_of_life_metal_acceleration_data
+  #endif
+) {
+  cexil_renderer_destroy(
+    renderer
+  );
+
+  #if with_metal == 1
+  game_of_life_metal_acceleration_destroy(
+    game_of_life_metal_acceleration_data
+  );
+  #endif
+
+  for (
+    unsigned int index_y = 0;
+    index_y < game_of_life_parameters->size.y;
+    ++index_y
+  ) {
+    free(
+      cells_next[
+        index_y
+      ]
+    );
+  }
+
+  free(
+    cells_next
+  );
 }
 
 #endif
