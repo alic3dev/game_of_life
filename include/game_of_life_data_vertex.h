5dc1e3ade20e1c3bb0349468cd3800ed8c762a6d
alic3 <alic3dev@gmail.com>
2026-05-20 16:25:52 -0700

    `include/*.h`:refactor

--- include/game_of_life_data_vertex.h
+++ include/game_of_life_data_vertex.h
@@ -1,7 +1,9 @@
 #include <math_c_vector.h>
 
 struct game_of_life_data_vertex {
-  float4 position [[position]];
+  float4 position [[
+    position
+  ]];
   float brightness;
   float4 colour;
   float rotation_colour;
--- include/game_of_life_metal_acceleration_data_error.h
+++ include/game_of_life_metal_acceleration_data_error.h
@@ -1,16 +1,16 @@
 #ifndef __game_of_life_metal_acceleration_data_error_h
 #define __game_of_life_metal_acceleration_data_error_h
 
-#define game_of_life_metal_acceleration_data_error_length 5
+#define game_of_life_metal_acceleration_data_error_length 0x05
 
 enum game_of_life_metal_acceleration_data_error {
-  game_of_life_metal_acceleration_data_error_none = 0,
-  game_of_life_metal_acceleration_data_error_create_metal_device = 1,
-  game_of_life_metal_acceleration_data_error_create_library = 2,
-  game_of_life_metal_acceleration_data_error_create_function_compute = 3,
-  game_of_life_metal_acceleration_data_error_create_pipeline = 4,
-  game_of_life_metal_acceleration_data_error_create_command_queue = 5,
-  game_of_life_metal_acceleration_data_error_create_buffer = 6
+  game_of_life_metal_acceleration_data_error_none                    = 0x00,
+  game_of_life_metal_acceleration_data_error_create_metal_device     = 0x01,
+  game_of_life_metal_acceleration_data_error_create_library          = 0x02,
+  game_of_life_metal_acceleration_data_error_create_function_compute = 0x03,
+  game_of_life_metal_acceleration_data_error_create_pipeline         = 0x04,
+  game_of_life_metal_acceleration_data_error_create_command_queue    = 0x05,
+  game_of_life_metal_acceleration_data_error_create_buffer           = 0x06
 };
 
 extern const char* game_of_life_metal_acceleration_data_error_description[
