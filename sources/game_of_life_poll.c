e4d3e5679e7a861c988e42b4637dcab03746dbe9
alic3 <alic3dev@gmail.com>
2026-05-20 15:51:34 -0700

    `game_of_life_poll`

--- sources/game_of_life_poll.c
+++ sources/game_of_life_poll.c
@@ -8,67 +8,163 @@ void game_of_life_poll(
   char** cells_next
 ) {
   for (
-    unsigned int index_y = 0;
-    index_y < game_of_life_parameters->size.y;
+    unsigned int index_y = (
+      0x00
+    );
+    (
+      index_y <
+      game_of_life_parameters->size.y
+    );
     ++index_y
   ) {
     for (
-      unsigned int index_x = 0;
-      index_x < game_of_life_parameters->size.x;
+      unsigned int index_x = (
+        0x00
+      );
+      (
+        index_x <
+        game_of_life_parameters->size.x
+      );
       ++index_x
     ) {
-      unsigned int living_neighbors = 0;
+      unsigned int living_neighbors = (
+        0x00
+      );
 
       for (
-        unsigned int index_neighbour_y = index_y == 0 ? 1 : index_y - 1;
-        index_neighbour_y < index_y + 2;
+        unsigned int index_neighbour_y = (
+          (
+            index_y ==
+            0x00
+          )
+          ? 0x01
+          : (
+            index_y -
+            0x01
+          )
+        );
+        (
+          index_neighbour_y <
+          (
+            index_y +
+            0x02
+          )
+        );
         ++index_neighbour_y
       ) {
         for (
-          unsigned int index_neighbour_x = index_x == 0 ? 1 : index_x - 1;
-          index_neighbour_x < index_x + 2;
+          unsigned int index_neighbour_x = (
+            (
+              index_x ==
+              0x00
+            )
+            ? 0x01
+            : (
+              index_x -
+              0x01
+            )
+          );
+          (
+            index_neighbour_x <
+            (
+              index_x +
+              0x02
+            )
+          );
           ++index_neighbour_x
         ) {
           if (
-            (index_neighbour_y == index_y && index_neighbour_x == index_x) ||
-            index_neighbour_y >= game_of_life_parameters->size.y ||
-            index_neighbour_x >= game_of_life_parameters->size.x
+            (
+              index_neighbour_y ==
+              index_y
+            ) &&
+            (
+              index_neighbour_x ==
+              index_x
+            ) ||
+            (
+              index_neighbour_y >=
+              game_of_life_parameters->size.y
+            ) ||
+            (
+              index_neighbour_x >=
+              game_of_life_parameters->size.x
+            )
           ) {
             continue;
           }
 
           if (
-            cells[index_neighbour_y][index_neighbour_x] == 1
+            cells[
+              index_neighbour_y
+            ][
+              index_neighbour_x
+            ] ==
+            0x01
           ) {
             living_neighbors = (
-              living_neighbors + 1
+              living_neighbors +
+              0x01
             );
           }
         }
       }
 
       if (
-        (cells[index_y][index_x] == 1 && living_neighbors == 2) ||
-        living_neighbors == 3
+        (
+          cells[
+            index_y
+          ][
+            index_x
+          ] ==
+          0x01
+        ) &&
+        (
+          living_neighbors ==
+          0x02
+        ) ||
+        (
+          living_neighbors ==
+          0x03
+        )
       ) {
-        cells_next[index_y][index_x] = 1;
+        cells_next[
+          index_y
+        ][
+          index_x
+        ] = (
+          0x01
+        );
       } else {
-        cells_next[index_y][index_x] = 0;
+        cells_next[
+          index_y
+        ][
+          index_x
+        ] = (
+          0x00
+        );
       }
     }
   }
 
   for (
-    unsigned int index_y = 0;
-    index_y < game_of_life_parameters->size.y;
+    unsigned int index_y = (
+      0x00
+    );
+    (
+      index_y <
+      game_of_life_parameters->size.y
+    );
     ++index_y
   ) {
     clic3_bytes_copy(
-      cells[index_y],
-      cells_next[index_y], (
-        sizeof(unsigned char) *
-        game_of_life_parameters->size.x
-      )
+      cells[
+        index_y
+      ],
+      cells_next[
+        index_y
+      ],
+      game_of_life_parameters->size.x
     );
   }
 }
